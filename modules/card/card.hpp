33e44a35e2883b2d708c5e25b6948f1875b9fc76
unknown <ferindyaulia19@gmail.com>
2023-03-12 15:34:01 +0700

    update header card

--- modules/card/card.hpp
+++ modules/card/card.hpp
@@ -1,4 +1,48 @@
-#ifdef CARD_HPP_
+#ifndef CARD_HPP_
 #define CARD_HPP_
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class GameCard{
+    public:
+        // constructor
+        GameCard();
+
+        // destructor
+        ~GameCard();
+
+        // getter
+        int get_number();
+        char get_color();
+
+        // setter
+        void set_number(int);
+        void set_color(char);
+
+    private:
+        vector<int> number;
+        vector<char> color;
+
+};
+
+class AbilityCard{
+    public:
+        // constructor
+        AbilityCard();
+
+        // destructor
+        ~AbilityCard();
+
+        // getter
+        int get_ability();
+
+        // setter
+        void set_ability(int);
+
+    private:
+        vector<int> ability;
+};
+
 
 #endif
